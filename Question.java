18c51f04c7ed3adcc3a1405e82a30bef59942d7e
Parul Sangwan15 <parul.sangwan1501@gmail.com>
2025-03-04 01:40:44 +0530

    Added quiz questions

--- Question.java
+++ Question.java
@@ -7,6 +7,18 @@ public class Question {
     private String opt3;
     private String opt4;
     private String answer;
+
+    
+
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String answer) {
+        this.id = id;
+        this.question = question;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.answer = answer;
+    }
     public int getId() {
         return id;
     }
@@ -48,6 +60,17 @@ public class Question {
     }
     public void setAnswer(String answer) {
         this.answer = answer;
+    }
+
+    @Override
+    public String toString(){
+        return "Question no. " + getId() + " is: \n" + getQuestion() + 
+        "\nOptions are: " + "\n" + getOpt1() +
+        "\n" + getOpt2() +
+        "\n" + getOpt3() +
+        "\n" + getOpt4() +
+        "\nThe Answer is: " + getAnswer();
+
     }
 
     
--- QuestionService.java
+++ QuestionService.java
@@ -1,6 +1,34 @@
 public class QuestionService {
      
 
-    Question[] questions = new Question[5];
+    Question[] questions = new Question[6];
+
+    public void displayQuestions(){
+        questions[0] = new Question(1, "What is this language?", "Java", "Python", "C#", "C++", "Java");
+        questions[1] = new Question(2, "Size of int?", "2", "6", "4", "8", "4");
+        questions[2] = new Question(3, "Colour of sky?", "Pink", "Blue", "It's just reflection", "Whatever I want to think!", "Whatever I want to think!");
+        questions[3] = new Question(4, "How do I feel today?", "Good", "Happy", "Confident", "All of the above:)", "All of the above:)");
+        questions[4] = new Question(5, "Okay, Size of boolean", "1", "2", "3", "4", "1");
+        questions[5] = new Question(6, "Size of long?", "2", "6", "4", "8", "8");
+
+        // for(int i = 0; i <6; i++){
+        // System.out.println("Question no. " + questions[i].getId() + " is: ");
+        // System.out.println(questions[i].getQuestion());
+        // System.out.println("Option 1: " + questions[i].getOpt1());
+        // System.out.println("Option 2: " + questions[i].getOpt2());
+        // System.out.println("Option 3: " + questions[i].getOpt3());
+        // System.out.println("Option 4: " + questions[i].getOpt4());
+        // System.out.println("Answer is: " + questions[i].getAnswer() + "\n");
+        // }
+
+        // Instead of this use ternary for loop
+
+        for(Question ques : questions){
+            System.out.println(ques);                 // but here toString() method will print some value@hashcode so we override the toString() method
+
+        }
+
+
+    }
 
 }
