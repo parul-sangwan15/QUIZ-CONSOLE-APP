424bd85c151d559d3d746d4c9df3fc04e06623a5
Parul Sangwan15 <parul.sangwan1501@gmail.com>
2025-03-04 02:26:08 +0530

    Functionality to take answer from user and calculate score

--- QuestionService.java
+++ QuestionService.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 public class QuestionService {
      
 
-    Question[] questions = new Question[6];
+    Question[] questions = new Question[5];
+    String[] selection = new String[5];
 
-    public void displayQuestions(){
+    public QuestionService(){
         questions[0] = new Question(1, "What is this language?", "Java", "Python", "C#", "C++", "Java");
         questions[1] = new Question(2, "Size of int?", "2", "6", "4", "8", "4");
         questions[2] = new Question(3, "Colour of sky?", "Pink", "Blue", "It's just reflection", "Whatever I want to think!", "Whatever I want to think!");
-        questions[3] = new Question(4, "How do I feel today?", "Good", "Happy", "Confident", "All of the above:)", "All of the above:)");
-        questions[4] = new Question(5, "Okay, Size of boolean", "1", "2", "3", "4", "1");
-        questions[5] = new Question(6, "Size of long?", "2", "6", "4", "8", "8");
+        questions[3] = new Question(4, "Okay, Size of boolean", "1", "2", "3", "4", "1");
+        questions[4] = new Question(5, "Size of long?", "2", "6", "4", "8", "8");
+    }
+
+    public void playQuiz(){
 
         // for(int i = 0; i <6; i++){
         // System.out.println("Question no. " + questions[i].getId() + " is: ");
@@ -23,12 +28,43 @@ public class QuestionService {
 
         // Instead of this use ternary for loop
 
+        int i = 0;
         for(Question ques : questions){
-            System.out.println(ques);                 // but here toString() method will print some value@hashcode so we override the toString() method
+            // System.out.println(ques);                
+            
+            // but here toString() method will print some value@hashcode so we override the toString() method
+
+            System.out.println("Question no. " + ques.getId() + " is: ");
+            System.out.println(ques.getQuestion());
+            System.out.println("Option 1: " + ques.getOpt1());
+            System.out.println("Option 2: " + ques.getOpt2());
+            System.out.println("Option 3: " + ques.getOpt3());
+            System.out.println("Option 4: " + ques.getOpt4());
+
+            @SuppressWarnings("resource")
+            Scanner sc = new Scanner(System.in);
+            selection[i] = sc.nextLine();
+            i++;
+        }
+
+        for(String s : selection){
+            System.out.println(s);
 
         }
 
-
     }
 
+
+    public void printScore() {
+        int score = 0;
+        for (int i = 0; i < questions.length; i++) {
+            Question que = questions[i];
+            String answer = que.getAnswer();
+            String userAnswer = selection[i];
+            if (answer.equals(userAnswer)) {
+                score++;
+            }
+        }
+        System.out.println("Your score is : " + score);
+    }
 }
